d4e10f4bc4e2f645a3aa2a77c8330d19e04faadd
Ruo Miyata <miyaty.ruo@gmail.com>
2026-01-17 00:10:58 +0900

    chore: add file

---
+++ velite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, defineCollection, s } from 'velite'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+import remarkEmoji from 'remark-emoji'
+import remarkSupersub from 'remark-supersub'
+
+const notes = defineCollection({
+    name: 'Note',
+    pattern: 'notes/**/*.md',
+    schema: s.object({
+        title: s.string().optional(),
+        slug: s.path(),
+        date: s.isodate().optional(),
+        updated: s.isodate().optional(),
+        content: s.markdown(),
+        excerpt: s.excerpt().optional(),
+        toc: s.toc().optional(),
+    }).transform(data => ({ ...data, permalink: `/notes/${data.slug}` }))
+})
+
+export default defineConfig({
+    root: 'content',
+    output: {
+        data: '.velite',
+        assets: 'dist/static',
+        base: '/static/',
+        name: '[name]-[hash:6].[ext]',
+        clean: true
+    },
+    collections: { notes },
+    markdown: {
+        remarkPlugins: [
+            [remarkGfm, { singleTilde: false }],
+            remarkMath,
+            remarkEmoji as any,
+            remarkSupersub,
+        ],
+        rehypePlugins: [
+            rehypeKatex,
+        ]
+    }
+})
