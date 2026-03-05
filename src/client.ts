0a69c7105fe46189686c876286323873f8987c99
Ruo Miyata <miyaty.ruo@gmail.com>
2026-03-05 12:30:06 +0900

    fix: reorder component imports for improved organization

--- src/client.ts
+++ src/client.ts
@@ -6,8 +6,14 @@
  */
 
 import './components/app/app-router.js';
+import './components/ui/article-header/article-header.js';
+import './components/ui/blockquote/blockquote.js';
+import './components/ui/callout/callout.js';
+import './components/ui/codeblock/codeblock.js';
+import './components/ui/code-group/code-group.js';
 import './components/ui/ol/ol.js';
 import './components/ui/divider/divider.js';
+import './components/ui/table/table.js';
 import './components/ui/translation/translation.js';
 import { initTranslationOrchestrator } from './components/ui/translation/translation-orchestrator.js';
 
