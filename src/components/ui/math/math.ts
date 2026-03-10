dc2f778faefaf74f6dad680682a2ef5ce3633f69
Ruo Miyata <miyaty.ruo@gmail.com>
2026-03-10 21:56:10 +0900

    fix: add class

--- src/components/ui/math/math.ts
+++ src/components/ui/math/math.ts
@@ -138,6 +138,19 @@ export class UiMath extends LitElement {
       color: inherit;
     }
 
+    .katex .katex-mathml {
+      position: absolute;
+      width: 1px;
+      height: 1px;
+      padding: 0;
+      border: 0;
+      margin: -1px;
+      overflow: hidden;
+      clip: rect(0 0 0 0);
+      clip-path: inset(50%);
+      white-space: nowrap;
+    }
+
     .math-error {
       display: grid;
       gap: var(--space-3, 12px);
