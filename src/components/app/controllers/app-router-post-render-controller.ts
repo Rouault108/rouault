1b51e5c1225f1d8a37ffa9a96c57a6186734c773
Ruo Miyata <miyaty.ruo@gmail.com>
2026-04-05 15:59:07 +0900

    fix: eslint error

--- src/components/app/controllers/app-router-post-render-controller.ts
+++ src/components/app/controllers/app-router-post-render-controller.ts
@@ -69,7 +69,7 @@ export class AppRouterPostRenderController implements ReactiveController {
       }
 
       await new Promise<void>((resolve) => {
-        window.addEventListener('load', () => resolve(), { once: true });
+        window.addEventListener('load', () => { resolve(); }, { once: true });
       });
     };
 
